d3ca34942555aa66ae7fc684890c0c2995721a12
salih kocamaz <salihkcmz@gmail.com>
2024-03-25 23:16:23 +0300

    Ana Form İsmi DEğiştirildi

--- RestaurantManegement/Forms/MainMenu.cs
+++ RestaurantManegement/Forms/MainMenu.cs
@@ -1,8 +1,8 @@
 namespace RestaurantManegement
 {
-    public partial class Form1 : Form
+    public partial class MainMenu : Form
     {
-        public Form1()
+        public MainMenu()
         {
             InitializeComponent();
         }
